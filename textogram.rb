667425992869f6bcab3f6d64bdbaca34f467aa98
Rodney Gelera <rodney.gelera@shopify.com>
2021-06-16 11:10:38 -0700

    pt 2 done

--- textogram.rb
+++ textogram.rb
@@ -1,7 +1,8 @@
 class Textogram
-  def initialize(text, case_sensitive: false, special: false, order_by_count: false)
+  def initialize(text, case_sensitive: false, special: false, order_by_count: false, by_words: false)
     @case_sensitive = case_sensitive
     @special = special
+    @by_words = by_words
     @histo = generate_histogram(text)
     sorted_histo(order_by_count: order_by_count)
   end
@@ -9,10 +10,16 @@ class Textogram
   def generate_histogram(text)
     histo = Hash.new(0)
     text = text.downcase unless @case_sensitive # lower case by default
-    text = text.gsub(/[^0-9A-Za-z]/, '') unless @special # remove special characters
+    text = text.gsub(/[^0-9A-Za-z\s]/, '') unless @special # remove special characters
     
-    text.gsub(/\s+/, "").split("").each do |c|
-      histo[c] += 1
+    unless @by_words
+      text.gsub(/\s+/, "").split("").each do |c|
+        histo[c] += 1
+      end
+    else
+      text.split(" ").each do |w|
+        histo[w] += 1
+      end
     end
     return histo
   end
@@ -28,8 +35,16 @@ class Textogram
   def to_s
     output = ''
     unless @histo.empty?
-        @histo.each do |k, v|
-          output << "%s %s\n" % [k, "*" * v]
+        unless @by_words
+          @histo.each do |k, v|
+            output << "%s %s\n" % [k, "*" * v]
+          end
+        else
+          # padding words with spaces
+          max_word_length = @histo.keys.max { |a, b| a.length <=> b.length }.length
+          @histo.each do |k, v|
+            output << "%-#{max_word_length}s %s\n" % [k, "*" * v]
+          end
         end
     else
       output << "Textogram is currently empty."
@@ -38,11 +53,11 @@ class Textogram
   end
 end
 
-texto = Textogram.new("HELLO World!", special: true, case_sensitive: true)
-puts "Outputting hash with special and case sensitive"
-puts texto.to_s
 texto = Textogram.new("HELLO World!")
 puts "Outputting hash sorted alphabetically"
+puts texto.to_s
+texto = Textogram.new("HELLO World!", special: true, case_sensitive: true)
+puts "Outputting hash with special and case sensitive"
 puts texto.to_s
 puts "Outputting hash sorted by value descending"
 texto.sorted_histo(order_by_count: true)
@@ -51,3 +66,15 @@ puts texto.to_s
 texto = Textogram.new("")
 puts "Outputting empty hash"
 puts texto.to_s
+
+puts "===="
+puts "Outputting histogram by words"
+texto = Textogram.new("HELLO World!")
+puts texto.to_s
+
+puts "Outputting histogram of textfile by words"
+file = File.open("acruelenigma-paul-bourget.txt")
+texto = Textogram.new(file.readlines.join, by_words: true, order_by_count: true) 
+output = File.open("output.txt", "w")
+output << texto.to_s
+output.close
